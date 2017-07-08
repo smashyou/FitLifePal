47354f6044e344c3f6c72a991ab582f4b66bd68c
John (Keun) You <smashyou@users.noreply.github.com>
2017-07-08 03:29:27 -0400

    Update BMICalculatorVC.m

--- BMICalculatorVC.m
+++ BMICalculatorVC.m
@@ -26,16 +26,6 @@
     [super didReceiveMemoryWarning];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 - (IBAction)massChanged:(id)sender
 {
     _weightSlider.minimumValue = 0;
