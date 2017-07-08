cfd7a4b0f2076bce244848af1c66c953b6608fb0
John (Keun) You <smashyou@users.noreply.github.com>
2017-07-08 03:30:46 -0400

    Update CountdownTimerVC.m

--- CountdownTimerVC.m
+++ CountdownTimerVC.m
@@ -41,16 +41,6 @@
     
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 - (void)updateTime
 {
     if(!self.running) return;
